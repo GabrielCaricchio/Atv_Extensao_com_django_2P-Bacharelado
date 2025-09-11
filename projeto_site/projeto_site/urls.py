256df444f656054c06759335ee2f44d34cbb537d
GabrielArrudaCaricchio <gabrielarrudacaricchio296@gmail.com>
2025-09-11 19:56:33 -0300

    fiz url

--- projeto_site/projeto_site/urls.py
+++ projeto_site/projeto_site/urls.py
@@ -5,6 +5,6 @@ from app_site import views
 #rota(url.py) > views.py > html
 #rota, view responsavel, nome de referência
 urlpatterns = [
-    path('', views.index, name='index')
-    path('', views.quiz, name='quiz')
+    path('', views.index, name='index'),
+    path('', views.quiz, name='quiz'),
 ]
